9c98d5308e476b46abd13059ad687d45fcddbee8
Sebastian Gonzato <sebastian_gonzato@protonmail.com>
2021-04-19 10:14:57 +0200

    Variable naming problems

--- src/run_spine_periods.jl
+++ src/run_spine_periods.jl
@@ -1,5 +1,5 @@
 """
-    run_spine_periods_ordering(db_url_in, db_url_out; with_optimizer)
+    run_spine_periods_ordering(url_in, url_out; with_optimizer)
 
 Solves an optimisation problem which selects (and orders) representative periods.
 
@@ -7,24 +7,24 @@ Specifying which optimisation method can be done from, and can be either:
 * `representative_periods` - selects representative_periods.
 * `representative_periods_ordering` - selects and orders representative periods.
 """
-function run_spine_periods(db_url_in::String, db_url_out::String; 
+function run_spine_periods(url_in::String, url_out::String; 
         with_optimizer=optimizer_with_attributes(
             Cbc.Optimizer, "logLevel" => 1, "ratioGap" => 0.01,
             "seconds" => 60*10
         )
     )
     @info "Reading database..."
-    using_spinedb(db_url_in; upgrade=true)
+    using_spinedb(url_in; upgrade=true)
 
     if representative_period_method(representative_period=first(representative_period())) == :representative_periods
         return run_spine_periods_selection(
-            db_url_in, db_url_out,
-            with_optimizer=optimizer
+            url_in, url_out,
+            with_optimizer=with_optimizer
         )
     elseif representative_period_method(representative_period=first(representative_period())) == :representative_periods_ordering
         return run_spine_periods_ordering(
-            db_url_in, db_url_out,
-            with_optimizer=optimizer
+            url_in, url_out,
+            with_optimizer=with_optimizer
         )
     else
         error("Please specify an optimisation method in the Spine database.")
